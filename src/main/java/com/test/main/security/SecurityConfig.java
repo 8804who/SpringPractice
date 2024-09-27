71c59894809b6c78ada3b1974fdd008336dbe2c2
KJS <8804who@naver.com>
2024-09-27 16:17:10 +0900

    401 오류 에러 페이지 적용

--- src/main/java/com/test/main/security/SecurityConfig.java
+++ src/main/java/com/test/main/security/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .exceptionHandling()
+                .authenticationEntryPoint(new MyAuthenticationEntryPoint())
                 .accessDeniedPage("/errorPage");
     }
 }
