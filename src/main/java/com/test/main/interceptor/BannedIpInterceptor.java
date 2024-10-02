10c1bbb79735e41a33854f9f4d025d023e9bca03
KJS <8804who@naver.com>
2024-10-02 10:17:01 +0900

    Interceptor 적용

---
+++ src/main/java/com/test/main/interceptor/BannedIpInterceptor.java
@@ -0,0 +1,16 @@
+package com.test.main.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
+public class BannedIpInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
